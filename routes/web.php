cec2cb6218a284ac94ecb91151c20a2f777ceb90
shen <santi.dionis@gmail.com>
2023-10-15 19:20:11 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, World!';
+});
+
+
+Route::get('/hello/{name}', function ($name) {
+    return 'Hello, '.$name;
+});
