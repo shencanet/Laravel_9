42849b0cf1cb5ff5247a32c889c4f928e575da7c
shen <santi.dionis@gmail.com>
2023-10-15 20:29:28 +0200

    ADD: templates blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@@extends('template')
+
+@section()
 <h1>Home</h1>
 </body>
 </html>
---
+++ resources/views/template.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Proyecto Web</title>
+</head>
+<body>
+    <p> <a href="">Home</a>
+        <a href="">Blog</a>
+    </p>
+    <br>
+    @yield('content')
+
+</body>
+</html>
