ef146018e1f31cd460227d50d2c3508a2702fcc3
shen <santi.dionis@gmail.com>
2023-10-20 21:29:07 +0200

    ADD: Routes blade

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/blog', function () {
 
 
 
-Route::get('/blog/{slug}', function ($slug) {
+Route::get('blog/{slug}', function ($slug) {
     $post = $slug;
     return view('post', ['post' => $post]);
 })->name('post');
