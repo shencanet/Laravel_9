4f2df4ec239288c25a0571326faf3177ffe9d9e1
shen <santi.dionis@gmail.com>
2023-10-27 21:58:05 +0200

    ADD: Controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(10)->create();
          \App\Models\Post::factory(80)->create();
+            \App\Models\Comment::factory(100)->create();    
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
