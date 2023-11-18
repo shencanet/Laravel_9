fc0464cc63f10c3c631d618b9686f1c10bd924ef
shen <santi.dionis@gmail.com>
2023-11-18 21:21:56 +0100

    ADD: INDEX.BLADE

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,24 +11,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                	
+
                 	<table class="mb-4">
                         @foreach($posts as $post)
                         <tr class="border-b border-gray-200 text-sm">
                             <td class="px-6 py-4">{{ $post->title }}</td>
-                            
+
                             <td class="px-6 py-4">
                                 <a href="{{ route('posts.edit', $post) }}" class="text-indigo-600">Editar</a>
                             </td>
                             <td class="px-6 py-4">
                             	<form action="{{ route('posts.destroy', $post) }}" method="POST">
-								    @csrf 
+								    @csrf //TOKEN SEGURIDAD
 								    @method('DELETE')
 
-								    <input 
-								    	type="submit" 
-								    	value="Eliminar" 
-								    	class=" bg-gray-800 text-white rounded px-4 py-2" 
+								    <input
+								    	type="submit"
+								    	value="Eliminar"
+								    	class=" bg-gray-800 text-white rounded px-4 py-2"
 								    	onclick="return confirm('¿Desea Eliminar?')"
 								    >
 								</form>
