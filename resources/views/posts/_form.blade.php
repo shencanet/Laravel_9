1ba1964c4d3cb818ac1c8719e1e243745515cd26
shen <santi.dionis@gmail.com>
2023-11-20 19:11:40 +0100

    ADD: INDEX.BLADE

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -15,6 +15,7 @@
 >
 
 <label class="uppercase text-gray-700 text-xs">Contenido</label>
+
 <span class="text-xs text-red-600">@error('body') {{ $message }} @enderror</span>
 
 <textarea name="body" class="rounded border-gray-200 w-full mb-4" rows="5">{{ old('body', $post->body) }}</textarea>
@@ -22,7 +23,7 @@
 <div class="flex justify-between items-center">
 
     <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
-    
+
 
     <input type="submit" value="Enviar" class="bg-gray-800 text-white rounded px-4 py-2">
 </div>
